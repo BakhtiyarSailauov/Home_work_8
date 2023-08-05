6c5939186ecacc6ace6f12f2f303e53a28ce9304
BAkhtiyar <cena61454@gmail.com>
2023-08-05 14:04:19 +0600

    asing

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ from starlette.responses import JSONResponse
 
 from .flowers_repository import FlowersRepository
 from .users_repository import UsersRepository
-from .models import User, Flower, UserRequest, FlowerRequest
-from .database import Base, engine, SessionLocal
+from models import User, Flower, UserRequest, FlowerRequest
+from database import Base, engine, SessionLocal
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
